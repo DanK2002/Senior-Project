efa5020b64dc960a0e4146d7f6520abce5dc25f3
robotfrommars <senato68@students.rowan.edu>
2024-03-05 11:15:47 -0500

    starter modified to add model work

---
+++ basic/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render, redirect
+from django.http import Http404
+from .models import Computed
+from django.utils import timezone
+from datetime import datetime
+
+# Create your views here.
+
+# Home
+def home(request):
+    return render(request, "basic/home.html")
+
+# Computation function
+def compute(request):
+    if request.method == 'POST':
+        try:
+            input = int(request.POST.get('input'))
+            precomputed = Computed.objects.filter(input=input)
+            if precomputed.count() != 0:  
+                # This was already computed, so look up answer
+                answer = precomputed[0].output
+                time_computed = precomputed[0].time_computed
+            else:
+                # Compute the answer
+                answer = input**2
+                time_computed = timezone.now()
+                # Save it into the database
+                computed = Computed(
+                    input=input, 
+                    output=answer,
+                    time_computed=time_computed
+                )
+                computed.save() # Store it into the database
+        except:
+            raise Http404(f"Invalid input: {request.POST.get('input')}")
+        return render (
+            request,
+            "basic/compute.html",
+            {
+                'input': input,
+                'output': answer,
+                'time_computed': time_computed
+            }
+        )
+    else:
+        # Return a blank form
+        return render(
+            request, 
+            'basic/compute.html'
+        )
+            
+#Search Function
+def search(request):
+    if request.method == 'POST':
+        try:
+            input = int(request.POST.get('input'))
+            precomputed = Computed.objects.filter(input=input)
+            if precomputed.count() != 0:  
+                # This was already computed, so look up answer
+                answer = precomputed[0].output
+                time_computed = precomputed[0].time_computed
+            else:
+                # redirect stuff here
+                return render(
+                    request, 
+                    'basic/search.html',
+                    {
+                        'input': input,
+                        'output': None,
+                        'searched': True,
+                    }
+                )       
+        except:
+            raise Http404(f"Invalid input: {request.POST.get('input')}")
+        return render (
+            request,
+            "basic/search.html",
+            {
+                'input': input,
+                'output': answer,
+                'searched': True,
+                'time_computed': time_computed
+            }
+        )
+    else:
+        # Return a blank form
+        return render(
+            request, 
+            'basic/search.html', 
+            {
+                'searched': False,
+            }
+        )
