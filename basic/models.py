20c2a2e91186a13e6c7d552804f6dece2340c22c
robotfrommars <senato68@students.rowan.edu>
2024-03-05 11:19:19 -0500

    "adjustments"

--- basic/models.py
+++ basic/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -11,5 +12,47 @@ class Computed(models.Model):
 
     def __str__(self):
         return(f"{self.input} -> {self.output}")
+    
+##Project models
+
+#employees
+class Employee(models.Model):
+    user = models.OneToOneField(User)
+    wage = models.FloatField(null = False)
+
+class Shift(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    employee = models.ForeignKey(Employee)
+
+
+# orders and food
+class Order(models.Model):
+    number = models.IntegerField()
+    time_est = models.DateTimeField()
+    time_submitted = models.DateTimeField()
+    time_completed = models.DateTimeField()
+    foods = models.ManyToManyField('Food', blank=False)
+    meals = models.ManyToManyField('Meal', blank=False)
+    price = models.FloatField(null=False)
+    employee_submitted = models.ForeignKey(Employee)
+
+class Meal(models.Model):
+    name  = models.CharField(max_length=100)
+    foods = models.ManyToManyField('Food', blank=False)
+    price = models.FloatField(blank = False)
+
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField(null = False)
+    category = models.CharField(max_length=100)
+    ingred = models.ManyToManyField('Ingredient', blank=False)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField(blank = False)
+
+    def __str__(self):
+        return self.name
 
 
