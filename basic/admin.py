cd971ee5829a6040443ab7aa9849b61ff0e388e2
robotfrommars <senato68@students.rowan.edu>
2024-03-06 16:45:48 -0500

    faker file and adjustment

--- basic/admin.py
+++ basic/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Computed
 from .models import Employee, Shift, Order, Meal, Food, Ingredient
+from .user import User
 
 # Register your models here.
 admin.site.register(Computed)
 
 ##Project Models
+admin.site.register(User, UserAdmin)
 admin.site.register(Employee)
 admin.site.register(Shift)
 admin.site.register(Order)
