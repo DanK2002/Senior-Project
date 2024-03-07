e1186ac20b21637e32deb513d422e86b2b33116f
robotfrommars <senato68@students.rowan.edu>
2024-03-07 16:07:13 -0500

    preliminary complete fake data file. pain in fakedata4

--- fakedata4.py
+++ fakedata4.py
@@ -1,15 +1,13 @@
 from faker import Faker
-import random
 from basic.models import Employee, Shift
 from datetime import timedelta
+import random
 from django.utils import timezone
 
 fake = Faker()
-print(1)
 
-employees = Employee.objects.all()
-print(2)
-for employee in employees:
-    print(3)
-    print(employee)
-    print(4)
+for employee in Employee.objects.all():
+    for _ in range(3):  # Generate 3 shifts for each employee
+            start_time = fake.date_time_between(start_date='-30d', end_date='now', tzinfo=timezone.get_current_timezone())  # Random start time within the last 30 days
+            end_time = start_time + timedelta(hours=random.randint(4, 12))  # End time is within 4-12 hours after start time
+            shift = Shift.objects.create(start=start_time, end=end_time, employee=employee)
