9ec6df4fd5b819f903316f48eb928021c3b63515
robotfrommars <senato68@students.rowan.edu>
2024-03-08 11:42:13 -0500

    orders not in the loop

--- fakedata_orders.py
+++ fakedata_orders.py
@@ -1,67 +1,74 @@
-import json
-from basic.models import Ingredient, Food, Meal, Order, Employee, User
-from datetime import timedelta, datetime
+from faker import Faker
+from datetime import timedelta
+from django.utils import timezone
+from basic.models import Food, Meal, Order, User
+
+fake = Faker()
+
+start_times = [fake.date_time_between(start_date='-1d', end_date='now', tzinfo=timezone.get_current_timezone()),
+               fake.date_time_between(start_date='-1d', end_date='now', tzinfo=timezone.get_current_timezone()),
+               fake.date_time_between(start_date='-1d', end_date='now', tzinfo=timezone.get_current_timezone()),]
+
+emp =  User.objects.exclude(username='senato68').filter().first()
+print("Employee submitted: ", emp)
 
 orders_data = [
     {
-        'number' : 1,
-        'time_est' : timedelta(minutes=9),
-        'time_submitted' : datetime(2024, 2, 28, 16, 55, 23, 562368),
-        'time_completed' : datetime(2024, 2, 28, 17, 6, 57, 298540),
-        'foods' : ['Krabby Patty', 'Krusty Dog', 'Fries'],
-        'meals' : [],
-        'price' : 13.97,
-        'employee_submitted' : 'marks71',
+        'number': 1,
+        'time_est': start_times[0] + timedelta(minutes=9),
+        'time_submitted': start_times[0],
+        'time_completed': start_times[0] + timedelta(minutes=9),
+        'foods': ['Krabby Patty', 'Krusty Dog', 'Fries'],
+        'meals': [],
+        'price': 13.97,
+        'employee_submitted': emp,
     },
     {
-        'number' : 2,
-        'time_est' : timedelta(minutes=6),
-        'time_submitted' : datetime(2024, 3, 2, 11, 24, 48, 595341),
-        'time_completed' : datetime(2024, 3, 2, 11, 45, 13, 762390),
-        'foods' : [],
-        'meals' : ['Good Meal'],
-        'price' : 9.99,
-        'employee_submitted' : 'marks71',
+        'number': 2,
+        'time_est': start_times[1] + timedelta(minutes=10),
+        'time_submitted': start_times[1],
+        'time_completed': start_times[1] + timedelta(minutes=9),
+        'foods': [],
+        'meals': ['Good Meal'],
+        'price': 9.99,
+        'employee_submitted': emp,
     },
     {
-        'number' : 3,
-        'time_est' : timedelta(minutes=16),
-        'time_submitted' : datetime(2024, 3, 6, 12, 5, 47, 178930),
-        'time_completed' : datetime(2024, 3, 6, 12, 12, 9, 762904),
-        'foods' : ['Steamed Hams', 'Steamed Hams'],
-        'meals' : ['Good Meal'],
-        'price' : 19.97,
-        'employee_submitted' : 'marks71',
+        'number': 3,
+        'time_est': start_times[2] + timedelta(minutes=3),
+        'time_submitted': start_times[2],
+        'time_completed': start_times[2] + timedelta(minutes=4),
+        'foods': ['Steamed Hams', 'Krusty Dog'],
+        'meals': ['Good Meal'],
+        'price': 19.97,
+        'employee_submitted': emp,
     }
 ]
 
-#Continually get error for user does not exist
+print("order made")
+
 for order_data in orders_data:
-    try:
-        user_obj = User.objects.get(username=order_data['employee_submitted'])
-        print(user_obj)
-    except Exception as e:
-        print(f"Error getting user: {e}")
-    
-    try:
-        order = Order(number = order_data['number'],
-                    time_est = order_data['time_est'],
-                    time_submitted = order_data['time_submitted'],
-                    time_completed = order_data['time_completed'], 
-                    price = order_data['price'], 
-                    employee_submitted = Employee.objects.get(user=user_obj))
-    except Exception as e:
-        print(f"Error creating order: {e}")
-    for food_name in order_data['foods']:
-        try:
-            food = Food.objects.filter(name=food_name).first()
-            order.foods.add(food)
-        except Exception as e:
-            print(f"Error adding food: {e}")
-    for meal_name in order_data['meals']:
-        try:
-            meal = Meal.objects.filter(meal=meal_name).first()
-            order.meals.add(meal)
-        except Exception as e:
-            print(f"Error adding Meal: {e}")
+    print("in the loop")
+    user_exists = User.objects.get(username=order_data['employee_submitted'])
+    print(user_exists)
+    print('username valid')
+    order = Order(number = order_data['number'],
+                  time_est = order_data['time_est'],
+                  time_submitted = order_data['time_subitted'],
+                  time_completed = order_data['time_completed'],
+                  price = order_data['price'],
+                  employee_submitted = user_exists)
+    print('order created')
     order.save()
+    print('order saved')
+    for food_name in order_data['foods']:
+        food = Food.objects.filter(name=food_name).first()
+        order.foods.add(food)
+    print('foods saved')
+    for meal_name in order_data['meals']:
+        meal = Meal.objects.filter(name=meal_name).first()
+        order.meals.add(meal)
+    
+    print('meals saved')
+    order.save()
+    print('order')
