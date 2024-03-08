3e47b87161d139fe8d6d63ae776313ed777d0644
robotfrommars <senato68@students.rowan.edu>
2024-03-07 19:18:41 -0500

    changes to fakedata4.py

--- fakedata4.py
+++ fakedata4.py
@@ -1,13 +1,17 @@
-from faker import Faker
-from basic.models import Employee, Shift
-from datetime import timedelta
 import random
+from faker import Faker
 from django.utils import timezone
+from datetime import timedelta
+from basic.models import Employee, Shift
 
 fake = Faker()
 
 for employee in Employee.objects.all():
-    for _ in range(3):  # Generate 3 shifts for each employee
-            start_time = fake.date_time_between(start_date='-30d', end_date='now', tzinfo=timezone.get_current_timezone())  # Random start time within the last 30 days
-            end_time = start_time + timedelta(hours=random.randint(4, 12))  # End time is within 4-12 hours after start time
+    for x in range(3):
+        start_time = fake.date_time_between(start_date='-30d', end_date='now', tzinfo=timezone.get_current_timezone())
+        end_time = start_time + timedelta(hours=random.randint(4, 12))
+        try:
             shift = Shift.objects.create(start=start_time, end=end_time, employee=employee)
+        except Exception as e:
+            print(f"Error creating shift: {e}")
+
